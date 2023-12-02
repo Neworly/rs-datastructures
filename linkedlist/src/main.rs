f2f868555f2dcbee8e76129d54fa70322393cf1f
Neworly <piocarlo393@gmail.com>
2023-12-02 12:11:26 +0100

    linkedlist-push

--- linkedlist/src/main.rs
+++ linkedlist/src/main.rs
@@ -1,4 +1,65 @@
+// Add, Sort, Remove, Traverse?
 
-fn main() {
-    println!("Hello, world!");
+use std::{cell::{Cell, RefCell, Ref}, pin, borrow::BorrowMut, ops::{Deref, DerefMut}};
+
+#[derive(Debug)]
+struct Body<T: Clone> { value: Option<T>, next: Option<Box<Body<T>>> }
+struct LinkedList<T: Clone> { body: Body<T>, len: Cell<usize> }
+
+impl<T: Clone> Body<T> {
+    fn new(head_value: T, last_value: T) -> Self { 
+        Body { 
+            value: Some(head_value), 
+            next: Some(Box::new(Body { value: Some(last_value), next: None }))
+        }
+    }
+
+    fn push(&mut self, value: T) {
+        let mut node = &mut self.next;
+        while let Some(next) = node {
+            let predict = &next.next;
+            if predict.is_none() {
+                let prev_val = next.value.clone();
+                next.value = Some(value);
+                next.next = Some(Box::new( Body { value: prev_val, next: None } ));
+                break;
+            }
+            node = &mut next.next;
+        }
+
+    }
+}
+
+impl<T: Clone> LinkedList<T> {
+    fn new(start: T, end: T) -> Self {
+        LinkedList { body: Body::new(start, end), len: Cell::new(2) }
+    }
+
+    fn len(&self) -> usize {
+        return self.len.get()
+    }
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod cases {
+    use crate::*;
+
+    #[test]
+    fn has_tail() {
+        let mut build = Body::new(5, 1);
+        build.push(4);
+        build.push(7);
+        build.push(4);
+        build.push(7);
+        build.push(4);
+        build.push(7);
+        build.push(4);
+        build.push(7);
+        
+        println!("{:?}", build);
+    }
+
+
 }
---
+++ linkedlist/target/debug/deps/linkedlist-18fb51a5032f3c79.d
@@ -0,0 +1,5 @@
+C:\Users\pioca\OneDrive\Desktop\Coding\git\rs-datastructures\linkedlist\target\debug\deps\linkedlist-18fb51a5032f3c79.exe: src\main.rs
+
+C:\Users\pioca\OneDrive\Desktop\Coding\git\rs-datastructures\linkedlist\target\debug\deps\linkedlist-18fb51a5032f3c79.d: src\main.rs
+
+src\main.rs:
